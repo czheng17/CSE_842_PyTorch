ddcbaad839ce66626b1b31cd14c349f8894126ae
Chen Zheng <zhengc12@msu.edu>
2022-10-03 17:04:46 -0400

    allennlp

---
+++ allennlp/semantic_role_labeling/run_srl.py
@@ -0,0 +1,21 @@
+### author: Chen Zheng
+### 10/03/2022
+
+##########################################################################################################################
+# Semantic Role Labeling (SRL) is the task of determining the latent predicate argument structure of a sentence 
+# and providing representations that can answer basic questions about sentence meaning, including who did what to whom, etc.
+
+# Model: SRL BERT
+# An implementation of a BERT based model, which is currently the state of the art single model 
+# for English PropBank SRL (Newswire sentences). It achieves 86.49 test F1 on the Ontonotes 5.0 dataset.
+##########################################################################################################################
+
+from allennlp.predictors.predictor import Predictor
+import allennlp_models.tagging
+
+predictor = Predictor.from_path("./structured-prediction-srl-bert.2020.12.15.tar.gz")
+output = predictor.predict(
+    sentence="You raise me up, so I can stand on mountains."
+)
+
+print(output)
